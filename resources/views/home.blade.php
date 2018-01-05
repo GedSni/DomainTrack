125cf84e5fbc57a0c5a0cc41a30725a49f1f29d9
Gediminas <ged.snicorius@gmail.com>
2018-01-05 13:28:10 +0200

    home.blade temporary fix

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -12,75 +12,81 @@
     </div>
     <div class="col-xs-4">
         <p class="tableHead">Day</p>
-        <p class="tableHead2">Updated {{ $dataDay[0]->day_update_date }}</p>
-        <div class="table-responsive">
-            <table class="table table-bordered table-curved table-hover table-striped">
-                <thead>
-                <tr>
-                    <th>Domain</th>
-                    <th class="text-right">Rank</th>
-                    <th class="text-right">Diff</th>
-                </tr>
-                </thead>
-                <tbody>
-                @for($i = 0; $i < count($dataDay); $i++)
+        @if(isset($dataDay[0]))
+            <p class="tableHead2">Updated {{ $dataDay[0]->day_update_date }}</p>
+            <div class="table-responsive">
+                <table class="table table-bordered table-curved table-hover table-striped">
+                    <thead>
                     <tr>
-                        <td>{{$dataDay[$i]->name}}</td>
-                        <td align="right">{{$dataDay[$i]->day_rank}}</td>
-                        <td align="right">{{$dataDay[$i]->day_diff}}</td>
+                        <th>Domain</th>
+                        <th class="text-right">Rank</th>
+                        <th class="text-right">Diff</th>
                     </tr>
-                @endfor
-                </tbody>
-            </table>
-        </div>
+                    </thead>
+                    <tbody>
+                    @for($i = 0; $i < count($dataDay); $i++)
+                        <tr>
+                            <td>{{$dataDay[$i]->name}}</td>
+                            <td align="right">{{$dataDay[$i]->day_rank}}</td>
+                            <td align="right">{{$dataDay[$i]->day_diff}}</td>
+                        </tr>
+                    @endfor
+                    </tbody>
+                </table>
+            </div>
+        @endif
     </div>
     <div class="col-xs-4">
         <p class="tableHead" >Week</p>
-        <p class="tableHead2">Updated {{ $dataWeek[0]->week_update_date }}</p>
-        <div class="table-responsive">
-            <table class="table table-bordered table-curved table-hover table-striped">
-                <thead>
-                <tr>
-                    <th>Domain</th>
-                    <th class="text-right">Rank</th>
-                    <th class="text-right">Diff</th>
-                </tr>
-                </thead>
-                <tbody>
-                @for($i = 0; $i < count($dataWeek); $i++)
+        @if(isset($dataWeek[0]))
+            <p class="tableHead2">Updated {{ $dataWeek[0]->week_update_date }}</p>
+            <div class="table-responsive">
+                <table class="table table-bordered table-curved table-hover table-striped">
+                    <thead>
                     <tr>
-                        <td>{{$dataWeek[$i]->name}}</td>
-                        <td align="right">{{$dataWeek[$i]->week_rank}}</td>
-                        <td align="right">{{$dataWeek[$i]->week_diff}}</td>
+                        <th>Domain</th>
+                        <th class="text-right">Rank</th>
+                        <th class="text-right">Diff</th>
                     </tr>
-                @endfor
-                </tbody>
-            </table>
-        </div>
+                    </thead>
+                    <tbody>
+                    @for($i = 0; $i < count($dataWeek); $i++)
+                        <tr>
+                            <td>{{$dataWeek[$i]->name}}</td>
+                            <td align="right">{{$dataWeek[$i]->week_rank}}</td>
+                            <td align="right">{{$dataWeek[$i]->week_diff}}</td>
+                        </tr>
+                    @endfor
+                    </tbody>
+                </table>
+            </div>
+        @endif
     </div>
     <div class="col-xs-4">
         <p class="tableHead" >Month</p>
-        <p class="tableHead2">Updated {{ $dataMonth[0]->month_update_date }}</p>
-        <div class="table-responsive">
-            <table class="table table-bordered table-curved table-hover table-striped">
-                <thead>
-                <tr>
-                    <th>Domain</th>
-                    <th class="text-right">Rank</th>
-                    <th class="text-right">Diff</th>
-                </tr>
-                </thead>
-                <tbody>
-                @for($i = 0; $i < count($dataMonth); $i++)
+        @if(isset($dataMonth[0]))
+            <p class="tableHead2">Updated {{ $dataMonth[0]->month_update_date }}</p>
+            <div class="table-responsive">
+                <table class="table table-bordered table-curved table-hover table-striped">
+                    <thead>
                     <tr>
-                        <td>{{$dataMonth[$i]->name}}</td>
-                        <td align="right">{{$dataMonth[$i]->month_rank}}</td>
-                        <td align="right">{{$dataMonth[$i]->month_diff}}</td>
+                        <th>Domain</th>
+                        <th class="text-right">Rank</th>
+                        <th class="text-right">Diff</th>
                     </tr>
-                @endfor
-                </tbody>
-            </table>
-        </div>
+                    </thead>
+                    <tbody>
+                    @for($i = 0; $i < count($dataMonth); $i++)
+                        <tr>
+                            <td>{{$dataMonth[$i]->name}}</td>
+                            <td align="right">{{$dataMonth[$i]->month_rank}}</td>
+                            <td align="right">{{$dataMonth[$i]->month_diff}}</td>
+                        </tr>
+                    @endfor
+                    </tbody>
+                </table>
+            </div>
+        @endif
     </div>
 </div>
 @endsection
