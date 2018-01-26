4cc8770c803c72c883a8beca723e002a6ee589aa
Gediminas <ged.snicorius@gmail.com>
2018-01-26 12:46:09 +0200

    Changing_database_structure

--- app/Console/Commands/drop.php
+++ app/Console/Commands/drop.php
@@ -38,10 +38,29 @@ class drop extends Command
      */
     public function handle()
     {
+        if (!$this->confirm('CONFIRM DROP AL TABLES IN THE CURRENT DATABASE? [y|N]')) {
+            exit('Drop Tables command aborted');
+        }
+
+        $colname = 'Tables_in_' . env('DB_DATABASE');
+
+        $tables = DB::select('SHOW TABLES');
+
+        foreach($tables as $table) {
+
+            $droplist[] = $table->$colname;
+
+        }
+        $droplist = implode(',', $droplist);
+
         DB::beginTransaction();
-        DB::statement('DROP TABLE ranks');
-        DB::statement('DROP TABLE domains');
-        DB::statement('DROP TABLE migrations');
+        //turn off referential integrity
+        DB::statement('SET FOREIGN_KEY_CHECKS = 0');
+        DB::statement("DROP TABLE $droplist");
+        //turn referential integrity back on
+        DB::statement('SET FOREIGN_KEY_CHECKS = 1');
         DB::commit();
+
+        $this->comment(PHP_EOL."If no errors showed up, all tables were dropped".PHP_EOL);
     }
 }
